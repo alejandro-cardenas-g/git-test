bcb04a9cffab02a9e362c517e56a8674823e9778
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2022-10-04 19:17:42 -0500

    feat/new-feature2

--- work.js
+++ work.js
@@ -4,5 +4,8 @@ const a = 1;
 const b = 2;
 const c = 3;
 
-console.log(index.sumArgs(1,2,3));
-console.log(index.subtractArgs(1,2,3));
+console.log(index.sumArgs(a,b,c));
+
+console.log(index.subtractArgs(a,b,c));
+
+console.log(index.multiply(a,b));
