8cdb96338463eeaeb925ce9b2ca8eac42924e832
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2022-10-04 19:08:38 -0500

    feat: add friend class

---
+++ classes/class1.js
@@ -0,0 +1,12 @@
+export class Friend{
+    _name;
+
+    constructor(name){
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+}
--- work.js
+++ work.js
@@ -1,4 +1,5 @@
 const index = require('./index');
+const { Friend } = require('./classes/class1');
 
 const a = 1;
 const b = 2;
@@ -6,3 +7,6 @@ const c = 3;
 
 console.log(index.sumArgs(1,2,3));
 console.log(index.subtractArgs(1,2,3));
+
+const friend = new Friend('Carlos');
+console.log(friend.name);
